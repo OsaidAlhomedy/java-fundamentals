37803e16f30069f6360bdf213ccab5ef60724e4f
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-09-29 10:09:13 +0300

    finished the whole thing

--- linter/app/src/main/java/linter/App.java
+++ linter/app/src/main/java/linter/App.java
@@ -3,17 +3,21 @@
  */
 package linter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import org.checkerframework.checker.units.qual.A;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+
 
 public class App {
 
 
     public static void main(String[] args) {
-
-
+        Path path = Path.of("app/src/main/resources/gate.js");
+        App newApp = new App();
+        System.out.println(newApp.linter(path));
 
     }
 
@@ -51,27 +55,28 @@ public class App {
 
     }
 
-    public String tally(List<String> list){
+    public String tally(List<String> list) {
 
         HashSet<String> uniques = new HashSet<>(list);
         HashMap<String, Integer> hashList = new HashMap<>();
 
-        for(String item : uniques){
-            hashList.put(item,0);
+        for (String item : uniques) {
+            hashList.put(item, 0);
         }
 
-        for (String vote : list){
-            if (hashList.containsKey(vote)){
+        for (String vote : list) {
+            if (hashList.containsKey(vote)) {
                 Integer x = hashList.get(vote);
                 x++;
-                hashList.put(vote,x);
-            };
+                hashList.put(vote, x);
+            }
+            ;
         }
 
         int counter = 0;
         String winner = "none";
-        for(String item : uniques){
-            if(hashList.get(item) > counter ){
+        for (String item : uniques) {
+            if (hashList.get(item) > counter) {
                 counter = hashList.get(item);
                 winner = item;
             }
@@ -81,6 +86,29 @@ public class App {
         return winner;
 
 
+    }
+
+    public String linter(Path path) {
+        int counter = 0;
+        StringBuilder errorLine = new StringBuilder();
+        try {
+            File myFile = new File(path.toUri());
+            Scanner scanner = new Scanner(myFile);
+
+            while (scanner.hasNextLine()) {
+                counter++;
+                String data = scanner.nextLine();
+                if (!data.endsWith(";") && !data.endsWith("}") && !data.endsWith("{") && !data.contains("else") && !data.contains("if") && !data.isEmpty() || data.contains("return") && !data.endsWith(";")) {
+                    errorLine.append("Line ").append(counter).append(" : Missing semicolon.\n");
+                }
+            }
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        return errorLine.toString();
     }
 
 
--- linter/app/src/test/java/linter/AppTest.java
+++ linter/app/src/test/java/linter/AppTest.java
@@ -3,9 +3,9 @@
  */
 package linter;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,4 +41,60 @@ class AppTest {
         String winner = classUnderTest.tally(votes);
         assertEquals("Bush received the most votes", winner + " received the most votes", "test failed we will get them next time");
     }
+
+/*
+    @Test
+    void linterTesting() {
+        App classUnderTesting = new App();
+        Path path = Path.of("app/src/main/resources/gate.js");
+       assertEquals("Line 3 : Missing semicolon.\n" +
+               "Line 5 : Missing semicolon.\n" +
+               "Line 11 : Missing semicolon.\n" +
+               "Line 13 : Missing semicolon.\n" +
+               "Line 15 : Missing semicolon.\n" +
+               "Line 26 : Missing semicolon.\n" +
+               "Line 28 : Missing semicolon.\n" +
+               "Line 32 : Missing semicolon.\n" +
+               "Line 36 : Missing semicolon.\n" +
+               "Line 40 : Missing semicolon.\n" +
+               "Line 41 : Missing semicolon.\n" +
+               "Line 42 : Missing semicolon.\n" +
+               "Line 50 : Missing semicolon.\n" +
+               "Line 51 : Missing semicolon.\n" +
+               "Line 59 : Missing semicolon.\n" +
+               "Line 60 : Missing semicolon.\n" +
+               "Line 61 : Missing semicolon.\n" +
+               "Line 62 : Missing semicolon.\n" +
+               "Line 64 : Missing semicolon.\n" +
+               "Line 70 : Missing semicolon.\n" +
+               "Line 71 : Missing semicolon.\n" +
+               "Line 72 : Missing semicolon.\n" +
+               "Line 73 : Missing semicolon.\n" +
+               "Line 74 : Missing semicolon.\n" +
+               "Line 76 : Missing semicolon.\n" +
+               "Line 77 : Missing semicolon.\n" +
+               "Line 78 : Missing semicolon.\n" +
+               "Line 79 : Missing semicolon.\n" +
+               "Line 80 : Missing semicolon.\n" +
+               "Line 82 : Missing semicolon.\n" +
+               "Line 83 : Missing semicolon.\n" +
+               "Line 84 : Missing semicolon.\n" +
+               "Line 85 : Missing semicolon.\n" +
+               "Line 86 : Missing semicolon.\n" +
+               "Line 88 : Missing semicolon.\n" +
+               "Line 89 : Missing semicolon.\n" +
+               "Line 90 : Missing semicolon.\n" +
+               "Line 91 : Missing semicolon.\n" +
+               "Line 92 : Missing semicolon.\n" +
+               "Line 94 : Missing semicolon.\n" +
+               "Line 95 : Missing semicolon.\n" +
+               "Line 96 : Missing semicolon.\n" +
+               "Line 97 : Missing semicolon.\n" +
+               "Line 98 : Missing semicolon.\n" +
+               "Line 99 : Missing semicolon.\n" +
+               "Line 100 : Missing semicolon.\n" +
+               "Line 101 : Missing semicolon.", classUnderTesting.linter(path));
+    }
+*/
+
 }
